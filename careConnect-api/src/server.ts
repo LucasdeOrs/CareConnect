45fef7bbe4eb948de992b98201aa2cc6f64f5806
LucasCareConnect <lucas.careconnect@gmail.com>
2025-10-22 15:15:09 -0300

    feat: implementa RF005 e RF006 (agendamentos - criar, listar e atualizar status)

--- careConnect-api/src/server.ts
+++ careConnect-api/src/server.ts
@@ -3,7 +3,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { supabase } from './config/supabaseClient';
 import authRoutes from './auth/authRoutes';
+<<<<<<< Updated upstream
 import userRoutes from './users/userRoutes';
+=======
+import appointmentRoutes from './appointments/appointmentRoutes';
+>>>>>>> Stashed changes
 
 dotenv.config();
 const app = express();
@@ -12,7 +16,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+<<<<<<< Updated upstream
 app.use('/usuarios', userRoutes);
+=======
+app.use('/agendamentos', appointmentRoutes);
+>>>>>>> Stashed changes
 
 app.get('/', (req, res) => {
   res.send('Consulta Fácil API funcionando!');
