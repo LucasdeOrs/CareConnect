6e645914d7adc520d1cdf893702fbf4c50ec70eb
LucasCareConnect <lucas.careconnect@gmail.com>
2025-10-22 14:44:46 -0300

    feat(auth): implementa cadastro e login com Supabase (RF001 e RF002)

--- careConnect-api/src/server.ts
+++ careConnect-api/src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { supabase } from './config/supabaseClient';
+import authRoutes from './auth/authRoutes';
+
 
 dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Consulta Fácil API funcionando!');
