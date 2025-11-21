64926cf39f23645cf9d1aebd5eeb195be2241c8c
LucasCareConnect <lucas.careconnect@gmail.com>
2025-11-21 14:03:51 -0300

    Fix

--- careConnect-api/src/auth/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { registerUser, loginUser } from './authController';
-
-const router = express.Router();
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-
-export default router;
